1c5f144333ad443d7a1d644f36ed5077423514af
Eli-Green <greene2@spu.edu>
2020-11-28 19:27:05 -0800

    added CORS configuration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var db = require('./database'); // Connect to the database
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/books', booksRouter); // Use that router using /api/friends
 
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
