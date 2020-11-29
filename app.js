2c0062099227ea8da50f07b518c51c940b76240c
Eli-Green <greene2@spu.edu>
2020-11-28 19:32:07 -0800

    installed npm cors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/books', booksRouter); // Use that router using /api/friends
 
-app.use(cors());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
